55eaa0f88177e90fe9e838cc1383834aa068df06
STONEAGE\adilson.souza <adilson.souza@stoneage.com.br>
2023-10-11 10:57:09 -0300

    commit 2

--- index.js
+++ index.js
@@ -1,20 +1,20 @@
 #!/usr/bin/env node
-import _ from 'lodash';
-import fs from 'fs';
-import path from 'path';
 import axios from 'axios';
+import fs from 'fs';
+import json2csvConverter from 'json-2-csv';
+import _ from 'lodash';
 import neatCsv from 'neat-csv';
+import path from 'path';
 import pdfToBase64 from 'pdf-to-base64';
 import logger from 'simple-node-logger';
 
 import { contractsConfig } from './config.js';
-import { isError } from 'util';
 
 async function main() {
     let _logger = null;
     let _proposalsData = [];
-
-    const _csvDocName = '0-100';
+    let dataToReport = [] ; // Adilson
+    const _csvDocName = '26001-27000';
     const _documentsName = {
         CARTAO: ['BGNTCCC', 'BGNCCBADCARTV6'],
         PORTABILIDADE: [
@@ -87,82 +87,70 @@ async function main() {
                 )}]`
             );
         }
-        _logger.info("proposalData response.data:: da proposta "+ proposal+ " == " + JSON.stringify(response.data))
 
         return response.data;
     }
-/////////// Adilson fim///////////
-async function generateDocuments(proposalDataArray) {
-    let index = 1;
+    /////////// Adilson fim///////////
+    async function generateDocuments(proposalDataArray) {
+        let index = 1;
+       // let dataToReport = [];// Adilson
 
-    for (let {
-        proposalId,
-        product,
-        contract,
-        // uuid,
-    } of proposalDataArray) {
-        proposalId = JSON.stringify(Object.values(proposalDataArray[index-1])[0]).replace(/"/g, "");
-        _logger.info(
-            `Processando proposta ${proposalId} (Processo ${index} de ${proposalDataArray.length})`
-        );
+        for (const {
+            proposalId,
+            contract,
+            product
+        } of proposalDataArray) {
+            _logger.info(
+                `Processando proposta ${proposalId} (Processo ${index} de ${proposalDataArray.length})`
+            );
 
-        try {
-            let dadosProposta = await _getProposalById(proposalId)
-            _logger.info(`dadosProposta linha 137 ${proposalId} :: ${JSON.stringify(dadosProposta)}`);
+            try {
+                let proposalData = await _getProposalById(proposalId);
 
-            if (dadosProposta.length === 0 ) {
-                _logger.info(`Propostqa  ${proposalId} não encontrada`);
-                return
+                if (proposalData.length === 0) {
+                    _logger.info(`Proposta ${proposalId} não encontrada`);
+                } else {
+                    proposalData = _.head(proposalData.filter(item => item.status === 'APPROVED' || (item.status === 'DERIVED' && item.statusMesa === 'APROVADO')))                    
+
+                    const documents = await _getDocumentsBySubscription(
+                        proposalId,
+                        product
+                    );
+
+                    const docPDF = _buildDocPDFdata(
+                        proposalId,
+                        proposalData.clientId,
+                        documents
+                    );
+
+                    proposalData.contract = contract;
+                    proposalData.product = product;
+                    proposalData.signedDocumentsUrl = await _getSignedDocumentsUrlRequest(
+                        proposalData.sessionId,
+                        proposalData.clientId,
+                        docPDF
+                    );
+
+                    // const signedDocumentsUrl =
+                    //     await _getSignedDocumentsUrlRequestMotorNovo(uuid);
+
+                    await _transformDocumentAndSendToP3(proposalData);
+
+                    dataToReport.push(proposalData);
+                }
+
+
+            } catch (error) {
+                _logger.error(`generateDocuments :: ${error}`);
+                _logger.error(`generateDocuments :: ${JSON.stringify(error)}`);
             }
 
-
-        // retorno: Sempre filtrar os status: APPROVED ou DERIVED e statusMesa: APROVADO
-        // Se houver os dois: filtrar pelo mais novo
-        dadosProposta = dadosProposta.filter(item => (item.status ==="DERIVED" || item.status === "APPROVED") 
-        && item.statusMesa === "APROVADO")
-
-        dadosProposta = dadosProposta.sort(
-            (objA, objB) => Number(new Date(objA.createdAt)) - Number(new Date(objB.createdAt)),
-        );
-
-        _logger.info(" Linha 85 : " + JSON.stringify(dadosProposta[0]))
-
-           //****** TESTANDO AGORA  ******/
-            const documents = await _getDocumentsBySubscription(
-                proposalId,
-                product
-            );
-
-            /*
-            const docPDF = _buildDocPDFdata(
-                proposalId,
-                clientId,
-                documents
-            );
-
-            const signedDocumentsUrl = await _getSignedDocumentsUrlRequest(
-                sessionId,
-                clientId,
-                docPDF
-            );
-            */
-
-            // const signedDocumentsUrl =
-            //     await _getSignedDocumentsUrlRequestMotorNovo(uuid);
-
-            // todo: descomentar quando tiver ok
-            // await _transformDocumentAndSendToP3(
-            //     signedDocumentsUrl,
-            //     contract
-            // );
-        } catch (error) {
-            _logger.error(`generateDocuments :: ${error}`);
-            _logger.error(`generateDocuments :: ${JSON.stringify(error)}`);
+            index++;
         }
 
-        index++;
+        await _buildReport(dataToReport);
     }
-}
+
     function createLogger() {
         return logger.createSimpleLogger({
             logFilePath: `./logs/${_csvDocName}.log`,
@@ -179,7 +167,7 @@ async function generateDocuments(proposalDataArray) {
                 }
 
                 const arrayData = await neatCsv(data, {
-                    separator: ';',
+                    separator: ';'
                 });
 
                 resolve(arrayData);
@@ -187,10 +175,8 @@ async function generateDocuments(proposalDataArray) {
         });
     }
 
-
-
     async function _getDocumentsBySubscription(proposalId, product) {
-        _logger.info(`Executando _getDocumentsBySubscription com dados :: proposalId: ${proposalId} e product: ${product}` )
+        _logger.info(`Executando _getDocumentsBySubscription com dados :: proposalId: ${proposalId} e product: ${product}`)
         const documents = [];
         const documentsName = _documentsName[product];
 
@@ -216,7 +202,7 @@ async function generateDocuments(proposalDataArray) {
                     `_getDocumentBySubscription :: Contrato não encontrado para proposta ${proposalId} e documento ${documentName}`
                 );
                 _logger.error(
-                    `Erro na API::  ${error} `
+                    `Erro na API:: ${error.message}`
                 );
             }
         }
@@ -233,12 +219,12 @@ async function generateDocuments(proposalDataArray) {
             {
                 headers: {
                     'Content-Type': 'application/json',
-                    useridentification: 'STONETEMP',
+                    useridentification: 'STONEAGE|Ct#242668',
                     callerReference: 'API',
                     requestId: '02443ef1-d4d3-40da-ab17-1b269271cdca',
                     conversationId: '10101010101',
                     Authorization:
-                        'Bearer eyJhbGciOiJIUzI1NiJ9.ew0KCSJwYXJ0bmVyX2lkIjogIjE4MTI0NzAwMDAwMTUzIiwNCiAgCSJkYXRhIjogImV5SmxibU1pT2lKQk1qVTJSME5OSWl3aVlXeG5Jam9pVWxOQkxVOUJSVkF0TWpVMkluMC5kVmVxWHYxekZPeE5tODU5djlLbi1IekY4TS0tWnA3dG1xaTQwRkF3enBOTVVpbHBSN2doSFJkNElsVG45d2R3cUFhbzA1eGZBWG11aDJiM2dIWGxrRGhUb28wY1l0TVhabUZhZllWVkJQZU5BM0ZEOGJ0VmRmV1FpOHVlQUlOcTN0RDJhbml3UzItVV94T0ZUaXFLV3hUWEFCUVluZjU1N2o1YjBKN0RiS2g4RVl2c29rVDZsT1l3REU0Ym1PM2pUUElwM2pscW8xdUFvdmhNXy1jUTZjcnQ0NEh6VUl5WUlpUDJnWkJRMnpsSmx3OHEta21Na25hS3ctekQwcW9ldjE0WGw4ZlRDMTJDX3dtaUJTV01ZMkdwOWxMYUR3bExXbURSSVpDWl9PekN1cFRrN1RrYUlWTDhucFREbmM2UElPQ1FTNWR3SUVDamZsM3VBa3p1eGcuc1ZhS3JLM2pHVU9fSkp4SC5BZnMzaFRnSjNWTWNLQ2IzODVlcklhZVQ0aFczRzVGUHBPZXRQYnlST0p4RFV2bjFNQXQ3SEt6cXl5MlVLTEE0QkRZOGZPMVNHUXl5ZVkwTk16RG55djY1Rkk1VFUwOUpTSzFJOVk0U0J4Wk9qQmdyanhHMk01bkRXS3NteWFmOFVHSU9DUjhUR09sTzNTZ2pNQlpfWldzUGhjX0ZCdkRZWWh3WmFnS3Z5dGRQaGtGdWFoTTVSRDI2T1lHSXN3LXpqQVpoRVVKcEJlczdTZWF1RDNyRndkb3ZHQmw5ZVFDejF0dGFNZG1nVUpKWVhwRjMzblVybHU5amIwVzNIUG5JNlRSV0V3VDBkaTg1UFQwTjE3a0RQQTZoVU9TSTdFQUVGMVNxdjdkbDM5UHJBX1BsNEZBUlYxSlM5MFdnT0s2YmpjbHduenIxZjN2N1VhdlpNTHc2cEdBZ3Y0T01TdDlTUGZFclJNaVV5eHpweUlVVTNTdnlEM1dEVHNDb2g2SVFQaS1KUWZEX2dtOTNvZzhjVWRqX0hqczhVcDBIcnJ4NU54RktKUFlOQ2xUVXdnTnpISUVuN21FcEwwWktzU2NFbUpTVmlhWmx3dGU3bmxMWUhLeEhwakowa0NURVBmVEhZZ2VFMWRKUEZaQlB6VXRBUlVZUERrYi1qM3pZZzZPVkszWGU4cU92SXl5QTBKR1Y5VDRwYUJ2bm1aM0hvREZOR1Q5RXc1WkU3ekRjNzB6aDM0MDhURGJsVk5WZEhNT0lGREJrODlLYmN1WGpLSWNJR0w4OTE3QkNmR0tlanAxWG5MbzRHZ0hrUXdoZWdjMGxvY0dmWEZUbDN4eW1heWhQM0xxbHF2Qm4zMkpOc3FzWmVUMlJJdXRkdlZuNHg3aUQ3QmpBWk5yLUptS0M2X0t3dWoxUjBZUTdxSkNQREpvd1doNk9SNDhLNlEzYmhoTXRKUS5JdFN2NVZLblR3WFUyUTFORmwyd1VRIg0KfQ0KDQoNCg0K.sMk004CzMvjLf5tDOZyWhEoB77Khlq-lIedB81qfArE',
+                        'Bearer eyJhbGciOiJIUzI1NiJ9.ew0KCSJwYXJ0bmVyX2lkIjogIjE4MTI0NzAwMDAwMTUzIiwNCiAgCSJkYXRhIjogImV5SmxibU1pT2lKQk1qVTJSME5OSWl3aVlXeG5Jam9pVWxOQkxVOUJSVkF0TWpVMkluMC5UbFlRbm1aTnN0YVBJLWtSSmRtc2hPSVFyRHh0ZlFjMk1YR1dwLThScVE5WlZrbjR2bEFKN3JUdlJweDl0YVk5X3FCTFl0dVM2NkNkaFZOVFV3V1NoVTNacGNjZ1FhemtIQjQtSEJNOUhoS3VBYmFsc0VGZmtKaGtCTFlhVGZvR2lPMVEtTXZzc0xWcXhCUWpJcVppcW5oU2ZiUnVRbDdaOVFNcTFOZ0dOdnI5VHNtOUFwNXdHbGY2SzFYZ0gyLWpBaVduUEpDUVBIZktrUU1iMk51NEZUOGhucmtpbElCcFVxQ2lCWHF5blpKRWVzUTV4dm1nMUZsbkxldVBPNzBhRi1PZno2UzZucURRRW9sbGZZWVE2ZkZPN3pUak5hQ294ZWh2RzBnS09qRFFVQkE5bmllcFJFTEt0V05RNUYxenFSTVV5T2pYWERfWGc3bENjTUhkaVEuQm1WbWtieEttYndYcF9rWS5xX2RuTUdSOV9xeXhGS3d4dWVwME1wZ3RqNE90UW4zT3ZjNXVsNDVrdHV5LUp5MVpSMzBJNld3SE12NURZV1pmekRMOGJ4UjRHSDQzYUVZRENDV0xlRFRyancxdWhWMU9WanAwNzlua2todXg3bUNydWwzazVwNUhxTjNuTjhNOFAyTDBBNmRnUi1mNklCTjRGY1dpdndDTTF2TXFMXzBSMjRoQl9ZQWRoemJfeGNWNExjNVhjNWF5ampYX2oyY2VGYnJNQ3pZdXR5bXVadE1fUnVHSGZmMExuMTJLMjJUeHlCd3FTcl9tU3FXb2VJTnJCaVhoVGtqRmtnTlRkX3d4YWVFZV9IUzM0M1RUR0lEaGV3amhncTdyWTNWcHhFZ1V3Rmd2S2k1eUNaQVA4b2hkNnZ1TjJyWUx2Y0hjSnFXdFV5Sm8zY1NvbkQ5XzRXRG54ZnN5S0V6VWlKeTdzdzlLaXh5am53dXY2aUl4TkJ6dV9ONG01RVY4NDVvOEhaTmNLR2NWSnlXUWlQUnBxblpzd3VqRUhJMDJmQzhGYTBEbkFQaktaN0M5U0tLdkUtY09PV1RtWHdKMXlUWDd4QWg4LUpEM0dFZVVscUkzSjdfX0tfSzFNelJsZ1N3TVdvajhHTDBMSTVrdjJzREVuV25nRVFtTjhKRWU5UHdWODF4TUhERlh0Szg0em5vUW9qc3dLTTZBNVFtV3F3NkgtaTk1V2YyNy1SOE1MNks3bnNjQ0FObHNyRGZMa1JCQUloaHpVYi0xRGNwWmN5Y0trOEpUMnhWbFllaXR3QWZkOEpOT05Hc2M0Y29QZDFyTzBaYzlqUjc5c2dKcVNuMFhkMFJ4cEhfZDYzOHVoYVZFYXhzSEZHVlRXcXZmTzZ4aGRfSmRkcVlEcDVDbXlZOXdmdFFIdGdYY1REU0ZUVlN3VEJGNGFTU2swbkR2OVJHaGdkbEhGZy5Obl9CUW1hQlFMSVVZOHlBWHVMdER3Ig0KfQ0KDQoNCg0K.dV76oPgkDnNKHP0Su8s8aSWOfMGmDpT2vO8z_nGEQb4',
                 },
             }
         );
@@ -292,15 +278,15 @@ async function generateDocuments(proposalDataArray) {
 
     async function _getSignedDocumentsUrlRequest(sessionId, clientId, docPDF) {
         const response = await axios.post(
-            'https://motor-core.cetelem.com.br/api/Script/Run',
+            'https://motor.cetelem.com.br/api/Script/Run',
             {
-                ScriptName: 'identify-assinatura-digital\\callback-pdf64',
+                ScriptName: 'processos-paralelos\\callback-pdf64',
                 ScriptParameters: {
                     sessionId,
                     clientId,
                     docPDF,
                 },
-                Token: '4d2448a3-f53b-4221-9117-590ad833f0c4',
+                Token: '59f9083e-6077-4bf6-a5a3-9cd2fbbdbc94',
             }
         );
 
@@ -354,7 +340,7 @@ async function generateDocuments(proposalDataArray) {
         return response;
     }
 
-    async function _transformDocumentAndSendToP3(signedDocumentsUrl, contract) {
+    async function _transformDocumentAndSendToP3({ signedDocumentsUrl, contract }) {
         for (const documentUrl of signedDocumentsUrl) {
             const fileName = path.basename(documentUrl);
             const base64 = await pdfToBase64(documentUrl);
@@ -377,6 +363,16 @@ async function generateDocuments(proposalDataArray) {
                 })),
             },
         ];
+    }
+
+    async function _buildReport(data) {
+        const csvFile = await json2csvConverter.json2csv(data, {
+            delimiter: {
+                field: ';'
+            }
+        })
+
+        fs.writeFileSync(`./reports/${_csvDocName}.csv`, csvFile);
     }
 
     function _fillContract(proposalData) {
